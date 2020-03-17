121c7f8c5733a5b08150bc468dff0fee44449734
lino vinayan <linovinm@gmail.com>
2020-03-17 15:18:20 +0530

    final output

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  exform: FormGroup;
+
+  ngOnInit() {
+
+  this.exform = new FormGroup({
+    'name' : new FormControl(null, Validators.required),
+    'email' : new FormControl(null, [Validators.required, Validators.email]),
+    'phone' : new FormControl(
+      null,
+      [
+        Validators.required,
+        Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')
+      ]),
+    'message' : new FormControl(null, [Validators.required, Validators.minLength(10)])
+  });
+  }
+
+  clicksub() {
+    console.log(this.exform.value);
+    this.exform.reset();
+  }
+  get name() {
+    return this.exform.get('name');
+  }
+  get email() {
+    return this.exform.get('email');
+  }
+  get phone() {
+    return this.exform.get('phone');
+  }
+  get message() {
+    return this.exform.get('message');
+  }
+
+}
